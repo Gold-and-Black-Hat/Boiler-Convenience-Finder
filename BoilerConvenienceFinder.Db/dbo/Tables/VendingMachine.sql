604d287234bc52b42e2cc6ce898136ea35b1c300
Steven Oliver Smith <smit3114@purdue.edu>
2020-01-12 15:07:10 -0500

    Added the database to the project

---
+++ BoilerConvenienceFinder.Db/dbo/Tables/VendingMachine.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[VendingMachine] (
+    [Id]                   INT          IDENTITY (1, 1) NOT NULL,
+    [Name]                 VARCHAR (50) NOT NULL,
+    [LocationId]           INT          NOT NULL,
+    [VendingMachineTypeId] INT          NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_VendingMachine_Location] FOREIGN KEY ([LocationId]) REFERENCES [dbo].[Location] ([Id]),
+    CONSTRAINT [FK_VendingMachine_VendingMachineType] FOREIGN KEY ([VendingMachineTypeId]) REFERENCES [dbo].[VendingMachineType] ([Id])
+);
+
---
+++ BoilerConvenienceFinder/BoilerConvenienceFinder/Services/BoilerConvienceFinderContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BoilerConvienceFinder.Data
+{
+	public class BoilerConvienceFinderContext : DbContext
+	{
+		//TODO: Make the connection string dynamic
+		protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlServer(@"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = C:\Source Control\Boiler-Convenience-Finder\BoilerConvenienceFinder\BoilerConvenienceFinder\Services\BoilerConvenienceFinder.mdf; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False");
+	}
+}
